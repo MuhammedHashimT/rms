7d266e56d09b3f4944ec2cb8f1509aa15b61d980
Sahl PP <99268671+fatahfreinds@users.noreply.github.com>
2023-08-23 12:46:47 +0000

    utils changed

--- src/candidates/candidates.service.ts
+++ src/candidates/candidates.service.ts
@@ -14,7 +14,7 @@ import { CandidateProgrammeService } from 'src/candidate-programme/candidate-pro
 import { Team } from 'src/teams/entities/team.entity';
 import { CreateInput } from './dto/create-input.dto';
 import { CredentialsService } from 'src/credentials/credentials.service';
-import { fieldsIdChecker, fieldsValidator } from 'src/utils/util';
+import { fieldsIdChecker, fieldsValidator } from '../utils/util';
 import { createReadStream } from 'fs';
 import { join } from 'path';
 import { Readable } from 'stream';
