69e38c682f26b099c3b0eac378228eff200a2162
Sahl PP <99268671+fatahfreinds@users.noreply.github.com>
2023-08-26 18:16:02 +0530

    Update create-candidate.input.ts

--- src/candidates/dto/create-candidate.input.ts
+++ src/candidates/dto/create-candidate.input.ts
@@ -1,83 +1,27 @@
-import { InputType, Int, Field, registerEnumType, ObjectType } from '@nestjs/graphql';
-import { IsNotEmpty, IsNumber } from 'class-validator';
-import { Model } from '../entities/programme.entity';
-
-export enum Mode {
-  STAGE = 'STAGE',
-  NON_STAGE = 'NON_STAGE',
-  OUTDOOR_STAGE = 'OUTDOOR_STAGE'
-}
-
-export enum Type {
-  SINGLE = 'SINGLE',
-  GROUP = 'GROUP',
-  HOUSE = 'HOUSE'
-}
-
-
-registerEnumType(Mode, {
-  name: 'Mode',
-});
-
-registerEnumType(Type, {
-  name: 'Type',
-});
-
-registerEnumType(Model, {
-  name: 'Model',
-});
+import { InputType, Int, Field } from '@nestjs/graphql';
+import { Gender } from '../entities/candidate.entity';
+import { IsEnum, IsInt, IsNotEmpty } from 'class-validator';
+import { IsFourCharactersWithNumbers } from 'src/utils/Validator';
 
 @InputType()
-export class CreateProgrammeInput {
-
-  @IsNotEmpty()
-  @Field()
-  programCode: string;
-
+export class CreateCandidateInput {
   @IsNotEmpty()
   @Field()
   name: string;
 
-  @IsNotEmpty()
-  @Field(()=> Mode)
-  mode: Mode;
+  @Field({ nullable: true })
+  class: string;
 
-  @IsNotEmpty()
-  @Field(()=> Type)
-  type: Type;
+  @Field(()=> Int , { nullable: true })
+  adno: number;
 
-  @IsNotEmpty()
-  @Field() 
-  model: Model;
-  
-  @Field(()=> Int , {nullable:true})
-  groupCount: number;
+  @IsFourCharactersWithNumbers({message:"chest number must be 4 characters and last 3 characters must be numbers"})
+  @Field({ nullable: true })
+  chestNO : string;
 
-  @IsNotEmpty()
-  @IsNumber()
-  @Field(()=> Int )
-  candidateCount: number;
+  @Field( { nullable: true })
+  team: string;
 
-  @Field({nullable:true})                                                                                           
-  date: string;
-
-  
-  @Field(()=> Int ,{nullable:true} )
-  venue: number;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @Field(()=> Int)
-  duration: number;
-
-  @IsNotEmpty()
-  @Field()
-  conceptNote: string;
-                                             
-  @Field()
-  skill: string ;
-
-  @IsNotEmpty()
-  @Field()
-  category : string;
+  @Field({ nullable: true })
+  category: string;
 }
