632512de5c6d61ba1830de679d0833263e3de5f0
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-10-05 22:13:25 +0530

    Update result-gen.service.ts

--- src/candidate-programme/result-gen.service.ts
+++ src/candidate-programme/result-gen.service.ts
@@ -271,18 +271,25 @@ export class ResultGenService {
     return CandidateProgramme;
   }
 
-  async generatePoint(CandidateProgramme: CandidateProgramme) {
+async generatePoint(CandidateProgramme: CandidateProgramme) {
+    console.log(CandidateProgramme);
+
     // giving the point of grade
     const grade: Grade = CandidateProgramme.grade;
+
     CandidateProgramme.point = 0;
 
     if (grade) {
+      const grageWithPoint = await this.gradeService.findOne(grade.id , ['id' , 'name' , 'pointSingle' , 'pointGroup' , 'pointHouse'])
       if (CandidateProgramme.programme.type == Type.SINGLE) {
         CandidateProgramme.point = grade.pointSingle;
+        CandidateProgramme.point = grageWithPoint.pointSingle;
       } else if (CandidateProgramme.programme.type == Type.GROUP) {
         CandidateProgramme.point = grade.pointGroup;
+        CandidateProgramme.point = grageWithPoint.pointGroup;
       } else if (CandidateProgramme.programme.type == Type.HOUSE) {
         CandidateProgramme.point = grade.pointHouse;
+        CandidateProgramme.point = grageWithPoint.pointHouse;
       }
     }
 
